39af6b94a44edf832bd647ee07acb17c6742e489
Path byte <62700063+pgalari@users.noreply.github.com>
2024-07-14 17:32:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
 
+
     return render_template('index.html', message=f"Error al generar el círculo lunar: {str(e)}\nDetalles: {repr(e)}")
 
 
